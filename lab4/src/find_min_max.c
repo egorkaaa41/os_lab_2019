4992f2a88a21bcbc0ebad20c4a6aeb3aafe027f1
egorkaaa41 <egor.eg05@yandex.ru>
2020-06-28 16:35:53 +0000

    done:

---
+++ lab4/src/find_min_max.c
@@ -0,0 +1,20 @@
+#include "find_min_max.h"
+
+#include <limits.h>
+
+struct MinMax GetMinMax(int *array, unsigned int begin, unsigned int end) {
+  struct MinMax min_max;
+  min_max.min = INT_MAX;
+  min_max.max = INT_MIN;
+
+  int i = 0;
+  for (i = begin; i < end; i++)
+  {
+      if (min_max.min > array[i])
+      min_max.min = array[i];
+      if (min_max.max < array[i])
+      min_max.max = array[i];
+  }
+  
+  return min_max;
+}
